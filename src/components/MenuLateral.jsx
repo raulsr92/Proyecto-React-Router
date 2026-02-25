8e068b2e5a72a5b70b4848f76ab639c323e6e730
Raul Sanchez R. <raul.sr92@gmail.com>
2026-02-25 15:02:04 -0500

    Lo avanzado en el capítulo 4 - hover con tailwind css

--- src/components/MenuLateral.jsx
+++ src/components/MenuLateral.jsx
@@ -6,47 +6,48 @@
 
     return(
         <>
-            <aside className="bg-[#FFFDEA] col-span-2 pt-15">
+            <aside className="bg-[#A86523] col-span-2 pt-15">
                 <ul className="flex flex-col gap-y-4.5">
                     {/*  Opción del menú 1 */}
                     <li>
-                        <a href="" className="text-[16px] font-semibold flex items-center gap-x-4.5 pl-10 py-3">
-                            <i className="fa-regular fa-user text-[25px] text-[#2050E4]  "></i>
-                            Mi Perfil
+                        <a href="" className="group text-[16px] font-semibold flex items-center gap-x-4.5 pl-10 py-3 hover:bg-[rgba(245,200,87,0.3)] ">
+                            <i className="fa-regular fa-user text-[25px] text-[#F2B50B] group-hover:text-[#FFE100]"></i>
+                            <p className=" text-[#FCEF91] group-hover:text-white ">Mi Perfil</p>
                         </a>
 
                     </li>
 
                     {/*  Opción del menú 2 */}
                     <li>
-                        <a href="" className="text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3" >
-                            <i className="fa-solid fa-wrench text-[25px] text-[#2050E4]"></i>
-                            Servicios Activos
+                        <a href="" className="group  text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3 hover:bg-[rgba(245,200,87,0.3)]" >
+                            <i className="fa-solid fa-wrench text-[25px] text-[#F2B50B] group-hover:text-[#FFE100]"></i>
+                            <p className=" text-[#FCEF91] group-hover:text-white ">Servicios Activos</p>
+
                         </a>
                     </li>
 
                     {/*  Opción del menú 3 */}
                     <li>
-                        <a href="" className="text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3">
-                            <i className="fa-solid fa-chart-line text-[25px] text-[#2050E4]"></i>
-                            Servicios Históricos
+                        <a href="" className="group text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3 hover:bg-[rgba(245,200,87,0.3)]">
+                            <i className="fa-solid fa-chart-line text-[25px] text-[#F2B50B] group-hover:text-[#FFE100]"></i>
+                            <p className=" text-[#FCEF91] group-hover:text-white ">Servicios Históricos</p>
                         </a>
 
                     </li>
 
                     {/*  Opción del menú 4 */}
                     <li>
-                        <a href="" className="text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3">
-                            <i className="fa-regular fa-bell text-[25px] text-[#2050E4]"></i>
-                            Notificaciones
+                        <a href="" className="group text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3 hover:bg-[rgba(245,200,87,0.3)]">
+                            <i className="fa-regular fa-bell text-[25px] text-[#F2B50B] group-hover:text-[#FFE100]"></i>
+                            <p className=" text-[#FCEF91] group-hover:text-white ">Notificaciones</p>
                         </a>
                     </li>
 
                     {/*  Opción del menú 5 */}
                     <li>
-                        <a href="" className="text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3">
-                            <i className="fa-regular fa-message text-[25px] text-[#2050E4]"></i>
-                            Mensajes
+                        <a href="" className="group text-[16px] font-semibold flex items-center gap-x-4.5 pl-10  py-3 hover:bg-[rgba(245,200,87,0.3)]">
+                            <i className="fa-regular fa-message text-[25px] text-[#F2B50B] group-hover:text-[#FFE100]"></i>
+                            <p className=" text-[#FCEF91] group-hover:text-white ">Mensajes</p>
                         </a>
                     </li>
                 </ul>
